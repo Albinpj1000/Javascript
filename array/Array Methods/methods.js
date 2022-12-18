ed3771816abfa22f8139df23e60751dce4522189
Albinpj1000 <110523986+Albinpj1000@users.noreply.github.com>
2022-12-18 20:06:52 +0530

    initial commit

---
+++ array/Array Methods/methods.js
@@ -0,0 +1,9 @@
+
+let color = ["black", "blue", "red", "orange", "yellow","blue"]
+let result = color.filter((x)=>(x.length>=5))
+console.log(result)
+
+
+
+// let arr = [2,14,6,28,100,3]
+// console.log(arr.sort());
