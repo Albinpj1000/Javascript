898de1a3ce1e737872d79f95453828022dfb95a2
Albinpj1000 <110523986+Albinpj1000@users.noreply.github.com>
2022-12-18 20:56:57 +0530

    for-in for-of

---
+++ array/Array Methods/for-In.js
@@ -0,0 +1,10 @@
+let student = {
+    fname:"albin",
+    lname:"xavier",
+    level:"intermediate",
+    stack:"mern",
+}
+
+for(let variable in student){
+    console.log(student[variable])
+}
---
+++ array/Array Methods/forOf.js
@@ -0,0 +1,6 @@
+let arr1 = [1,2,3,4,5,6,7,8,9,10]
+let sum=0
+for(let variable of arr1){
+sum=sum+variable
+}
+console.log(sum);
