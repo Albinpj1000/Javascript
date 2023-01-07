1f17291b99583a024f1ef3a4f4ea6142aa704ace
Albinpj1000 <110523986+Albinpj1000@users.noreply.github.com>
2023-01-07 20:58:26 +0530

    object declaration part 3, 4

--- excercise/object/practice.js
+++ excercise/object/practice.js
@@ -1,6 +1,6 @@
 // declaring objects
 
-//------------------------- part 1------------------------------
+// //------------------------- part 1------------------------------
 const rocket ={
     name:"appolo",
     fuel:100,
@@ -12,7 +12,7 @@ rocket.home="earth";
 rocket["dest"]="mars";
 console.log(rocket);
 
-//------------------------- part 2------------------------------
+// //------------------------- part 2------------------------------
 
 const rocket = new Object() //constructor method
 rocket.name="appolo";
@@ -23,8 +23,23 @@ console.log(rocket);
 
 
 //------------------------- part 3------------------------------
+const rocket ={
+    name:"appolo",
+    fuel:100,
+}
 
+const obj2=Object.create(rocket)
+console.log(obj2);                   // wont show properties
+console.log(Object.getPrototypeOf(obj2)); // properties are stored in prototype
+
+                 
 
 //------------------------- part 4------------------------------
-//------------------------- part 5------------------------------
-//------------------------- part 6------------------------------
+const obj4=Object.create({})
+Object.defineProperty(obj4,"book",{
+    value:"ikigai",
+    enumerable:true
+})
+console.log(obj4);
+
+
