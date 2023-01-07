83de0343da54a0d0543c5b6582cebc5424672a40
Albinpj1000 <110523986+Albinpj1000@users.noreply.github.com>
2023-01-07 19:27:09 +0530

    object declaration part 1, 2

---
+++ excercise/object/practice.js
@@ -0,0 +1,30 @@
+// declaring objects
+
+//------------------------- part 1------------------------------
+const rocket ={
+    name:"appolo",
+    fuel:100,
+}
+console.log(rocket);
+
+// adding properties
+rocket.home="earth";
+rocket["dest"]="mars";
+console.log(rocket);
+
+//------------------------- part 2------------------------------
+
+const rocket = new Object() //constructor method
+rocket.name="appolo";
+rocket.fuel=100
+rocket.dest="moon"
+
+console.log(rocket);
+
+
+//------------------------- part 3------------------------------
+
+
+//------------------------- part 4------------------------------
+//------------------------- part 5------------------------------
+//------------------------- part 6------------------------------
