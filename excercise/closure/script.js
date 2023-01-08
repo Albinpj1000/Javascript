00df5b89c4f25b166b4b5e05f672da96eee3e924
Albinpj1000 <110523986+Albinpj1000@users.noreply.github.com>
2023-01-08 18:19:51 +0530

    javascript prototype

---
+++ excercise/closure/script.js
@@ -0,0 +1,27 @@
+// function init(){
+//     var name="albin";
+//     console.log("first name");
+
+//     function sayName(){
+//         console.log(name);
+//         console.log("second name");
+//     }
+//     return sayName;
+// }
+
+// var value = init()
+// console.log(value);
+// value()
+
+
+// function doAddition(x){
+//     console.log("do addition");
+//     return function subAddition(y){
+//         console.log("sub addition");
+//         return x+y;
+//     }
+// }
+
+// var add5=doAddition(5)
+// add5(7)
+// console.log(add5(7));
--- excercise/prototype/script.js
+++ excercise/prototype/script.js
@@ -1,1 +1,21 @@
+var heros=["thor","antman"]
+
+
+Array.prototype.getHero = function(){
+    console.log("hey iam getting hero");
+}
+
+//injecting new function (feature) into arrays prototype
+//any arrays can access this method now, as it is now a added method to all array datatypes
+console.log(heros.getHero());
+
+
+Object.prototype.getFromObject = function(){
+    console.log("hey iam hitting parent obj");
+}
+
+//injecting new function (feature) into Objects(global Object) prototype
+//any can access this method, as it is now a property of global Object
+console.log(heros);
+
 
