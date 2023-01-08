e38f862eecacf105d0ab4f817c060953ae4ec10e
Albinpj1000 <110523986+Albinpj1000@users.noreply.github.com>
2023-01-08 18:31:30 +0530

    javascript prototype chaining/inheritance

--- excercise/prototype/script.js
+++ excercise/prototype/script.js
@@ -1,21 +1,52 @@
-var heros=["thor","antman"]
+// var heros=["thor","antman"]
 
 
-Array.prototype.getHero = function(){
-    console.log("hey iam getting hero");
+// Array.prototype.getHero = function(){
+//     console.log("hey iam getting hero");
+// }
+
+// //injecting new function (feature) into arrays prototype
+// //any arrays can access this method now, as it is now a added method to all array datatypes
+// console.log(heros.getHero());
+
+
+// Object.prototype.getFromObject = function(){
+//     console.log("hey iam hitting parent obj");
+// }
+
+// //injecting new function (feature) into Objects(global Object) prototype
+// //any can access this method, as it is now a property of global Object
+// console.log(heros);
+
+
+//--------------------------inheritane--------------------------
+
+const User = {
+    name: "top name",
+    email: "topuser@gmail.com"
 }
 
-//injecting new function (feature) into arrays prototype
-//any arrays can access this method now, as it is now a added method to all array datatypes
-console.log(heros.getHero());
-
-
-Object.prototype.getFromObject = function(){
-    console.log("hey iam hitting parent obj");
+const Teacher = {
+    makeVideos: true
 }
 
-//injecting new function (feature) into Objects(global Object) prototype
-//any can access this method, as it is now a property of global Object
-console.log(heros);
+const TeachingSupport = {
+    isAvailable: false,
+    __proto__:User
+}
 
+const TSAssistant = {
+    makeAssignment: 'JS assigment',
+    fullTime: true,
+    __proto__:TeachingSupport     
+    //adding second object to first objects prototype, 
+    //again another object can also be added inside the prototype of second obj
+    // and it can be keep on chaining. prototype chaining
+                                
+}
 
+// different syntax
+TeachingSupport.__proto__ = User
+
+//modern syntax
+Object.setPrototypeOf(TeachingSupport,User)
